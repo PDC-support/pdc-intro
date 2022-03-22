8cd037f291676c6ef3cd0bba8d5c67a927d81c33
Xavier Aguilar Fruto <xaguilar@kth.se>
2022-03-22 17:10:47 +0100

    Added source code for SLURM exercise 1

---
+++ SLURM_exercises/hello_mpi.c
@@ -0,0 +1,19 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  int world_size,world_rank;
+
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
+  // Get the number of processes
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  // Get the rank of the process
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  // Print off a hello world message
+  printf("Hello world from rank %d out of %d process\n", world_rank, world_size);
+  // Finalize the MPI environment.
+  MPI_Finalize();
+  return 0;
+  }
+
