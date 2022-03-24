8f590b24ffb93d91076fa847bf29f4f8cba57373
Xin Li <lixin.reco@gmail.com>
2022-03-24 12:47:27 +0100

    updated exercises

--- COMPILE_exercises/matmul_mpi_omp_test.py
+++ COMPILE_exercises/matmul_mpi_omp_test.py
@@ -5,17 +5,17 @@ import time
 comm = MPI.COMM_WORLD
 nranks = comm.Get_size()
 
-nrows = 1000000
-ncols = 1000
+nrows = 2000000
+ncols = 2000
 
 local_matrix = np.random.rand(nrows // nranks, ncols)
 
+comm.barrier()
 t0 = time.time()
 
 mat = np.matmul(local_matrix.T, local_matrix)
-
 mat = comm.reduce(mat, op=MPI.SUM, root=0)
 
 if comm.Get_rank() == 0:
     print(f'Time spent in matmul: {time.time() - t0:.3f} sec')
-    print(np.max(np.abs(mat - mat.T)))
+    print('Error:', np.max(np.abs(mat - mat.T)))
