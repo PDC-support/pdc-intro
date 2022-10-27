5a6ef6d8e99f1a8269e5695e3503e9740727937c
xaguilar <xaguilar@kth.se>
2022-10-27 10:06:00 +0200

    Added reservation to exercise 2

--- SLURM_exercises/exercise-2.sh
+++ SLURM_exercises/exercise-2.sh
@@ -5,6 +5,9 @@
 # not required if you have set a default allocation
 #SBATCH -A edu2210.intropdc
 
+# Reservation for today
+#SBATCH --reservation=intropdc-2022-10-28
+
 # The name of the script is myjob
 #SBATCH -J myjob
 
