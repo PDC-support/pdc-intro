5f46cb1c60916bc55ea7e6d8b1c053b421010286
Xin Li <lixin.reco@gmail.com>
2024-03-19 10:51:00 +0100

    further updated compile-on-cpu

--- COMPILE_exercises/job-n1.sh
+++ COMPILE_exercises/job-n1.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 #SBATCH -A ...
-#SBATCH --reservation ...
 #SBATCH -p shared
 #SBATCH -t 00:10:00
-#SBATCH -J job-n4
+#SBATCH -J job-n1
 
 #SBATCH --nodes=1
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=8
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
 
 ml PDC/23.03
 ml cpeGNU/23.03
 ml cray-python/3.9.13.1
 
+export OMP_NUM_THREADS=16
 export OMP_PLACES=cores
+echo $SLURM_NTASKS MPI x $OMP_NUM_THREADS OMP
+srun python3 matmul_mpi_omp_test.py
+
+export OMP_NUM_THREADS=8
+export OMP_PLACES=cores
+echo $SLURM_NTASKS MPI x $OMP_NUM_THREADS OMP
+srun python3 matmul_mpi_omp_test.py
 
 export OMP_NUM_THREADS=4
-echo $SLURM_NTASKS MPI x $OMP_NUM_THREADS OMP
-srun python3 matmul_mpi_omp_test.py
-
-export OMP_NUM_THREADS=2
-echo $SLURM_NTASKS MPI x $OMP_NUM_THREADS OMP
-srun python3 matmul_mpi_omp_test.py
-
-export OMP_NUM_THREADS=1
+export OMP_PLACES=cores
 echo $SLURM_NTASKS MPI x $OMP_NUM_THREADS OMP
 srun python3 matmul_mpi_omp_test.py
--- COMPILE_exercises/job-n2.sh
+++ COMPILE_exercises/job-n2.sh
@@ -13,16 +13,17 @@ ml PDC/23.03
 ml cpeGNU/23.03
 ml cray-python/3.9.13.1
 
-export OMP_PLACES=cores
-
 export OMP_NUM_THREADS=8
+export OMP_PLACES=cores
 echo $SLURM_NTASKS MPI x $OMP_NUM_THREADS OMP
 srun python3 matmul_mpi_omp_test.py
 
 export OMP_NUM_THREADS=4
+export OMP_PLACES=cores
 echo $SLURM_NTASKS MPI x $OMP_NUM_THREADS OMP
 srun python3 matmul_mpi_omp_test.py
 
 export OMP_NUM_THREADS=2
+export OMP_PLACES=cores
 echo $SLURM_NTASKS MPI x $OMP_NUM_THREADS OMP
 srun python3 matmul_mpi_omp_test.py
